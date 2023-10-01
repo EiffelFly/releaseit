07b5f8f2fe0e269f74f9c0cacc74cead37016012
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-10-01 19:57:33 +0800

    feat: add tailwind typography in the editor

---
+++ src/components/editor/Editor.tsx
@@ -0,0 +1,19 @@
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+const Editor = () => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Hello World! 🌎️</p>",
+    editorProps: {
+      attributes: {
+        class:
+          "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none",
+      },
+    },
+  });
+
+  return <EditorContent editor={editor} />;
+};
+
+export default Editor;
